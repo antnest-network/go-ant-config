07ee6f6e26f2c64702a33c07f02e86f98f6f6a9e
antnest <antnest.network@gmail.com>
2021-11-15 09:28:27 +0800

    update config

--- ant.go
+++ ant.go
@@ -2,6 +2,8 @@ package config
 
 var DefaultQueenAddresses = []string{
 	"/ip4/13.57.3.254/tcp/4001/p2p/12D3KooWSHbYhw3MeVHGQU2nYUprREDkgiYqNbzpcvQdjiPRuLew",
+	"/ip4/3.11.8.114/tcp/4001/p2p/12D3KooWBRevRigHtU5x7WEpoLTFb7fsSWNnAk7LY7RM6Brn4dBW",
+	"/ip4/16.163.58.59/tcp/4001/p2p/12D3KooWELoezRGrWa5LLvzb1x7VekJ5exrz5SdSUUyGquVmxZ5f",
 }
 
 type Ant struct {
